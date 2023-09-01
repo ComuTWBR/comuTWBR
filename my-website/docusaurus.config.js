022dcee47c9d016606b59221a5d75108b44aa47c
ComuTWBR <techwritingbr@gmail.com>
2023-09-01 20:10:19 -0300

    update

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -6,20 +6,21 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Tech Writing Brasil',
+  tagline: 'Descomplicando o complexo',
   favicon: 'img/favicon.ico',
 
+  
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://comunidade-tech-writing-brasil.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/<comuTWBR>/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ComuTWBR', // Usually your GitHub org/user name.
+  projectName: 'comuTWBR', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
