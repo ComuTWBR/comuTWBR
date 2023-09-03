9e11a212e5d1c79b4d6dc5d0e3e46faa126dd8ed
ComuTWBR <techwritingbr@gmail.com>
2023-09-03 10:14:42 -0300

    update config

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -12,16 +12,17 @@ const config = {
 
   
   // Set the production url of your site here
-  url: 'https://comunidade-tech-writing-brasil.com',
+  url: 'https://comutwbr.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/<comuTWBR>/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ComuTWBR', // Usually your GitHub org/user name.
   projectName: 'comuTWBR', // Usually your repo name.
-
+  trailingSlash: false,
+  deploymentBranch: 'main',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
